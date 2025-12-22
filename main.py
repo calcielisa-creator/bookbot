36ab785337b65355b00c8a8c9d26c406be13b013
calcielisa-creator <calcielisa@gmail.com>
2025-12-22 20:33:14 +0100

    print content function: get book text

---
+++ main.py
@@ -0,0 +1,8 @@
+def get_book_text (path):
+    with open(path) as f:
+        content = f.read()
+    return content
+def main ():
+    f = get_book_text("books/frankenstein.txt")
+    print(f)
+main ()
