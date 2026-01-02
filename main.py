d45fffd6d52025471237e69b5d2816898e4b6841
calcielisa-creator <calcielisa@gmail.com>
2026-01-02 15:07:36 +0100

    count words function

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def get_book_text (path):
     with open(path) as f:
         content = f.read()
     return content
+def count_words (text):
+    num= len(text.split())
+    return num
 def main ():
     f = get_book_text("books/frankenstein.txt")
-    print(f)
+    num_words = count_words(f)
+    print("Found", num_words, "total words")
 main ()
