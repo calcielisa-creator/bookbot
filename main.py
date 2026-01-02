a1f76ebee3f19f399ca5a97777ae5e5fc9d3ee00
calcielisa-creator <calcielisa@gmail.com>
2026-01-02 17:18:42 +0100

    added source file with count words

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import count_words
 def get_book_text (path):
     with open(path) as f:
         content = f.read()
     return content
-def count_words (text):
-    num= len(text.split())
-    return num
 def main ():
     f = get_book_text("books/frankenstein.txt")
     num_words = count_words(f)
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words (text):
+    num= len(text.split())
+    return num
