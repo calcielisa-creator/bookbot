572f118fe0462cbaf017a85b7092ade538dcf0e9
calcielisa-creator <calcielisa@gmail.com>
2026-01-02 18:09:40 +0100

    added characters counter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_chars
 def get_book_text (path):
     with open(path) as f:
         content = f.read()
@@ -6,5 +6,7 @@ def get_book_text (path):
 def main ():
     f = get_book_text("books/frankenstein.txt")
     num_words = count_words(f)
+    counter = count_chars(f)
     print("Found", num_words, "total words")
+    print(counter)
 main ()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def count_words (text):
     num= len(text.split())
     return num
+def count_chars (text):
+    text = text.lower()
+    counter = {}
+    for i in range(0, len(text)):
+        if text[i] in counter:
+            counter[text[i]] += 1
+        else:
+            counter[text[i]] = 1  
+    return counter
